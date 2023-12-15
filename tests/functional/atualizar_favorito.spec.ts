4eb4c7513e0f2b3e475e51ba7e8b705f43587f14
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2023-12-15 09:35:59 -0300

    Modificações banco e put

--- tests/functional/atualizar_favorito.spec.ts
+++ tests/functional/atualizar_favorito.spec.ts
@@ -1,5 +1,13 @@
 import { test } from '@japa/runner'
 
 test.group('Atualizar favorito', () => {
-  // Write your test here
+  test('atualizar favorito', async ({client})=>{
+    const resposta=await client.put('/favoritos/1').json(
+      {'nome':'IFRN', 
+      'url':'www.ifrn.edu.br', 
+      'importante':false
+    })
+    resposta.assertStatus(200)
+  })
+
 })
